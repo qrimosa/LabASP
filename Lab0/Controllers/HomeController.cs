f0b468c4e43a1f300fbc15b0eb8fd97f20a60a27
qrimosa <maratiskhakov9@gmail.com>
2025-10-07 18:23:39 +0200

    Lab01 no2

--- Lab0/Controllers/HomeController.cs
+++ Lab0/Controllers/HomeController.cs
@@ -24,6 +24,44 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult About()
+    {
+        return View();
+    }
+
+    public IActionResult Age(int? date)
+    {
+        ViewBag.Result = $"{DateTime.Now.Year - date}";
+        return View();
+    }
+    
+    public IActionResult Calculator(double? x, double? y, [FromQuery(Name = "operator")]string op)
+    {
+        if (x is null || y is null)
+        {
+            return View("CalculatorError","Provide both x and y!" );
+        }
+        switch (op)
+        {
+            case "add":
+                ViewBag.Result = $"{x} + {y} = {x+y}";
+                break;
+            case "sub":
+                ViewBag.Result = $"{x} - {y} = {x-y}";
+                break;
+            case "mul":
+                ViewBag.Result = $"{x} * {y} = {x*y}";
+                break;
+            case "div":
+                ViewBag.Result = $"{x} / {y} = {x/y}";
+                break;
+            default:
+                ViewBag.Result = "Nieznany operator";
+                break;
+        }
+        return View();
+    }
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
